7bc1449fe4f509c24336a01f68e92ba842c66295
Jeff Liang <liangj@Jeffs-MacBook-Pro-4.local>
2024-03-11 14:50:19 -0700

    feat: add cbs sub modules

---
+++ cbs/main.go
@@ -0,0 +1,13 @@
+package main
+import (
+
+"fmt"
+"github.com/JeffLiang2018/go-submodules/ipcMsg"
+
+)
+
+
+func main() {
+	fmt.Println("This is cbs submodules. Will call ipcMsg")
+	ipcMsg.OutputString()
+}
