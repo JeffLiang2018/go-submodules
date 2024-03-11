fdb952a8e42d48ec126e80a826705896e5143d4e
Jeff Liang <liangj@Jeffs-MacBook-Pro-4.local>
2024-03-11 14:23:48 -0700

    Add submodule pcm

---
+++ pcm/main.go
@@ -0,0 +1,12 @@
+package pcm
+
+import (
+
+"fmt"
+"github.com/JeffLiang2018/go-submodules/ipcMsg"
+)
+
+func main() {
+	fmt.Println("This is pcm submodules. It would use ipcMsg modules")
+	ipcMsg.OutputString()
+}
