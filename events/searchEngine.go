10aa1e31b6008edb957ce0d3343ed7cf6b33fdc0
Jeff Liang <liangj@Jeffs-MacBook-Pro-4.local>
2024-03-13 13:38:56 -0700

    feat: add version for events module

--- events/searchEngine.go
+++ events/searchEngine.go
@@ -4,8 +4,11 @@ import (
 "github.com/JeffLiang2018/go-submodules/ipcMsg"
 )
 
+const EVENTS_VERSION = "v0.0.3"
+
+
 func SearchAllEvents() string {
 	fmt.Println("This is events/searchEngine!!!!!!")
 	fmt.Println(ipcMsg.OutputString())
-	return "From events/SearchEngine.go SearchAllEvents returns " + ipcMsg.OutputString()
+	return "From events/SearchEngine.go "+ EVENTS_VERSION + " SearchAllEvents returns " + ipcMsg.OutputString()
 }
